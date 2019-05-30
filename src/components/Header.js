45f2d40a871ea15e2994fbf7f060e683d1c827c6
Ryan Doney <ryandoney@rd-mb.local>
2019-05-30 15:26:53 -0500

    AlbumList component, axios setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-const Header = () => <Text>Albums</Text>;
+const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  textStyle: {
+    fontSize: 20,
+  },
+};
+
+const Header = props => {
+  const { textStyle, viewStyle } = styles;
+  const { headerText } = props;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{headerText}</Text>
+    </View>
+  );
+};
 
 export default Header;
